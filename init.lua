368b56e31ac48c883b0ab86d99dd132f2b2288e1
Sam Precious <samwdp@gmail.com>
2024-04-15 11:38:03 +0100

    fix: formatting

--- init.lua
+++ init.lua
@@ -154,6 +154,23 @@ require("lazy").setup(
                 },
             },
         },
+        {                       -- Useful plugin to show you pending keybinds.
+            "folke/which-key.nvim",
+            event = "VimEnter", -- Sets the loading event to 'VimEnter'
+            config = function() -- This is the function that runs, AFTER loading
+                require("which-key").setup()
+
+                -- Document existing key chains
+                require("which-key").register({
+                    ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+                    ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+                    ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+                    ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+                    ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+                    ["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
+                })
+            end,
+        },
         { -- Fuzzy Finder (files, lsp, etc)
             "nvim-telescope/telescope.nvim",
             event = "VimEnter",
@@ -270,6 +287,7 @@ require("lazy").setup(
                         map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
                         map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
                         map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+                        map("<C-,>", vim.lsp.buf.code_action, "[C]ode [A]ction")
                         map("<leader>f", vim.lsp.buf.format, "[F]ormat Document")
                         map("K", vim.lsp.buf.hover, "Hover Documentation")
                         map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
