9d488b530eac4ee5d40ea079e2698eb7b5be7771
Sam Precious <samwdp@gmail.com>
2024-05-13 13:31:15 +0100

    fix: fixed not having the leader key above lazy

--- after/ftplugin/cs.lua
+++ after/ftplugin/cs.lua
@@ -1,23 +1,24 @@
+local ose = require('omnisharp_extended')
 vim.keymap.set("n", "<leader>gd",
     function()
-        require('omnisharp_extended').telescope_lsp_definition()
+        ose.telescope_lsp_definition()
     end,
     { buffer = 0, desc = "[G]oto [D]efinition (decompile)" })
 
 vim.keymap.set("n", "<leader>gr",
     function()
-        require('omnisharp_extended').telescope_lsp_references()
+        ose.telescope_lsp_references()
     end,
     { buffer = 0, desc = "[G]oto [R]eferences (decompile)" })
 
 vim.keymap.set("n", "<leader>gt",
     function()
-        require('omnisharp_extended').telescope_lsp_type_definition()
+        ose.telescope_lsp_type_definition()
     end,
     { buffer = 0, desc = "[G]oto Type Definition (decompile)" })
 
 vim.keymap.set("n", "<leader>gi",
     function()
-        require('omnisharp_extended').telescope_lsp_implementation()
+        ose.telescope_lsp_implementation()
     end,
     { buffer = 0, desc = "[G]oto [I]mplementation (decompile)" })
--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -67,7 +67,7 @@ return {
         "folke/todo-comments.nvim",
         event = "VimEnter",
         dependencies = { "nvim-lua/plenary.nvim" },
-        opts = { signs = false },
+        opts = { signs = true },
     },
     {
         "norcalli/nvim-colorizer.lua",
