e2a4c02ca642bb51d476bfc4a757a9adfd0d5db5
Sam Precious <samwdp@gmail.com>
2024-05-21 20:45:40 +0100

    feat: removed neogit as its stopped working

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -1,22 +1,5 @@
 return {
     {
-    "NeogitOrg/neogit",
-    dependencies = {
-        "nvim-lua/plenary.nvim",      -- required
-        "sindrets/diffview.nvim",     -- optional - Diff integration
-
-        -- Only one of these is needed, not both.
-        "nvim-telescope/telescope.nvim",     -- optional
-        "ibhagwan/fzf-lua",                  -- optional
-    },
-    config = function()
-        -- init.lua
-        local neogit = require('neogit')
-        neogit.setup {}
-        vim.keymap.set('n', '<leader>gg', neogit.open, {})
-    end
-    },
-    {
         "tpope/vim-fugitive",
         config = function()
             vim.keymap.set('n', '<leader>gs', ':Git<CR>', {})
