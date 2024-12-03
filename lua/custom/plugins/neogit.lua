ccab35f281d855aef518c06bba83c5cacef2064a
Sam Precious <samwdp@gmail.com>
2024-12-03 12:39:02 +0000

    feat(neogit): added neogit

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -1,8 +1,20 @@
 return {
     {
-        "tpope/vim-fugitive",
-        config = function()
-            vim.keymap.set('n', '<leader>gs', ':Git<CR>', {desc =  "[G]it [S]tatus"})
-        end
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+        },
+        config = true
     }
+
+    -- {
+    --     "tpope/vim-fugitive",
+    --     config = function()
+    --         vim.keymap.set('n', '<leader>gs', ':Git<CR>', {desc =  "[G]it [S]tatus"})
+    --     end
+    -- }
 }
