0aa5ee11434761557ad3df8cf4dfb7088485967a
Sam Precious <samwdp@gmail.com>
2025-02-07 08:41:29 +0000

    fixed compiler extra functions

--- lua/custom/compiler.lua
+++ lua/custom/compiler.lua
@@ -5,18 +5,17 @@ local extension_to_compile_command = {
     csproj = "dotnet build ",
     sln = "dotnet build ",
     zig = "zig build run",
-    zon = "zig build run",
     odin = "odin run ",
     rs = "cargo run",
     go = "go run ",
     py = "python3 ",
-    lua = "lua ",
 }
 
+local types = {"cs", "sln", "csproj", "rs", "go", "odin", "zig" }
 function M.compile_command()
     -- get all files in current working directory and put into a variable called files
     -- local files = vim.fn.globpath(vim.fn.getcwd(), "**/*.{lua}", false, true)
-    local files = vim.fn.glob("**/*.{cs,rs,go,odin,zig,lua}", false, true)
+    local files = vim.fn.glob("**/*.{" .. table.concat(types, ",") .. "}", false, true)
     -- sort files by length where shortest is first
     table.sort(files, function(a, b)
         return #a < #b
